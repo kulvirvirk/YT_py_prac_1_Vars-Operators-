88628a86e8318dd9c8755b024105119762d95856
KVirkDev <kvirkdev@gmail.com>
2020-06-19 04:03:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+
+
+# 1. Assign the number 10 to a variable called my_fav_number. 
+
+# 2. Create a variable called userName and assign string value of "Kartar"
+
+# 3. Assign the string 'Punjab' to a variable called myHome and use the print function to print the value of userName. 
