8356bcab8797e475f96fbdfedde7087036a17041
KVirkDev <kvirkdev@gmail.com>
2020-06-19 04:11:12 +0000

    added practice examples

--- main.py
+++ main.py
@@ -4,4 +4,30 @@
 
 # 2. Create a variable called userName and assign string value of "Kartar"
 
-# 3. Assign the string 'Punjab' to a variable called myHome and use the print function to print the value of userName. 
+# 3. Assign the string 'Punjab' to a variable called myHome and use the print function to print the value of myHome.
+ 
+# 4. Guess the output of given code before running it: 
+num1 = 5 
+NUM1 = 7 
+print(num1)
+print(NUM1)
+
+# 5. Explain what is wrong with the following code: 
+1num = 7 + 5
+
+# 6. Guess the output of given code before running it: 
+a = 17
+b = 12
+a = b 
+print(a)
+
+# 7. Guess the output of given code before running it: 
+x, y = 5, 4
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+print(x**y)
+
