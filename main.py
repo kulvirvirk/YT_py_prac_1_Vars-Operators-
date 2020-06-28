79a9d84ae12ec9ab7acf28605544ee4e93870fcf
KVirkDev <kvirkdev@gmail.com>
2020-06-28 05:24:51 +0000

    more practice questions added

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 
-
-# 1. Assign the number 10 to a variable called my_fav_number. 
+# 1. Assign the number 10 to a variable called my_fav_number.
+my_fav_number = 10
 
 # 2. Create a variable called userName and assign string value of "Kartar"
+userName = 'Kartar'
 
 # 3. Assign the string 'Punjab' to a variable called myHome and use the print function to print the value of myHome.
- 
+myHome = "Punjab"
+print(myHome)
+
 # 4. Guess the output of given code before running it: 
 num1 = 5 
 NUM1 = 7 
@@ -13,21 +16,12 @@ print(num1)
 print(NUM1)
 
 # 5. Explain what is wrong with the following code: 
-1num = 7 + 5
+num = 7 + 5
+print(num)
+print('====================')
 
 # 6. Guess the output of given code before running it: 
 a = 17
 b = 12
 a = b 
 print(a)
-
-# 7. Guess the output of given code before running it: 
-x, y = 5, 4
-print(x+y)
-print(x-y)
-print(x*y)
-print(x/y)
-print(x//y)
-print(x%y)
-print(x**y)
-
